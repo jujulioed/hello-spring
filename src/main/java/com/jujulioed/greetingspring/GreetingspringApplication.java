d4640b7c100ddc09de3d886b399652cbc8345a9e
jujulioed <julioeduardon9@hotmail.com>
2025-12-02 20:58:12 -0300

    Initiate the project

---
+++ src/main/java/com/jujulioed/greetingspring/GreetingspringApplication.java
@@ -0,0 +1,13 @@
+package com.jujulioed.greetingspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreetingspringApplication.class, args);
+	}
+
+}
